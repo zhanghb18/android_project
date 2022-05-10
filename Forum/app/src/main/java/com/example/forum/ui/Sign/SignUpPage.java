a6c725cc5b0942a682ac57af85f4d37cd617a773
Wuuu-x <wuyx19@mails.tsinghua.edu.cn>
2022-05-10 20:44:48 +0800

    跳转到有fragment的页面

--- Forum/app/src/main/java/com/example/forum/ui/Sign/SignUpPage.java
+++ Forum/app/src/main/java/com/example/forum/ui/Sign/SignUpPage.java
@@ -1,10 +1,13 @@
 package com.example.forum.ui.Sign;
 
 import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -13,6 +16,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.forum.ForumActivity;
 import com.example.forum.R;
 //import com.example.forum.network.Sign;
 import com.google.android.material.snackbar.Snackbar;
@@ -73,20 +77,34 @@ public class SignUpPage extends Fragment {
         editTexts.add(view.findViewById(R.id.editTextPassword));
         editTexts.add(view.findViewById(R.id.editTextVerifyPassword));
         //Button verifyButton = view.findViewById(R.id.verify_button);
-        view.findViewById(R.id.SignUp_return).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(SignUpPage.this)
-                        .navigate(R.id.action_SignUpFragment_to_InitFragment);
-            }
-        });
+//        view.findViewById(R.id.SignUp_return).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                NavHostFragment.findNavController(SignUpPage.this)
+//                        .navigate(R.id.action_SignUpFragment_to_InitFragment);
+//            }
+//        });
         boolean[] isVerified = {false};
-        view.findViewById(R.id.rightarrow_Icon).setOnClickListener(v -> {
-            if (!judgeInfoCompleted()) {
-                // TODO
-                return;
+        view.findViewById(R.id.rightarrow_Icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputMethodManager im = (InputMethodManager) v.getContext()
+                        .getSystemService( Context.INPUT_METHOD_SERVICE );
+                if (im.isActive()){
+                    im.hideSoftInputFromWindow(v.getApplicationWindowToken(), 0);
+                }
+//                String username = username_edit.getText().toString();
+//                String pwd = pwd_edit.getText().toString();
+//                if (username == null || username.length() <= 0 || pwd == null || pwd.length() <=0){
+//                    Snackbar.make(view, "Please fill your information.", Snackbar.LENGTH_LONG)
+//                            .setAction("Action", null).show();
+//                    return;
+//                }
+                // 关闭之前的所有activity 进入Seino主程序
+                Intent intent = new Intent(getContext(), ForumActivity.class);
+                getContext().startActivity(intent);
+                //Sign.Login(getContext(), view, username, pwd);
             }
-            signup();
         });
     }
 
@@ -122,15 +140,15 @@ public class SignUpPage extends Fragment {
         return true;
     }
 
-    private void verify(Runnable successRun) {
-        EditText editText = getView().findViewById(R.id.editTextEmail);
-        Sign.Verify(getContext(), getView(), editText.getText().toString(), successRun);
-    }
-
-    private void signup() {
-        // TODO
-        Sign.SignUp(getContext(), getView(), editTexts.get(0).getText().toString(),
-                editTexts.get(2).getText().toString(), editTexts.get(1).getText().toString(),
-                editTexts.get(4).getText().toString());
-    }
+//    private void verify(Runnable successRun) {
+//        EditText editText = getView().findViewById(R.id.editTextEmail);
+//        Sign.Verify(getContext(), getView(), editText.getText().toString(), successRun);
+//    }
+//
+//    private void signup() {
+//        // TODO
+//        Sign.SignUp(getContext(), getView(), editTexts.get(0).getText().toString(),
+//                editTexts.get(2).getText().toString(), editTexts.get(1).getText().toString(),
+//                editTexts.get(4).getText().toString());
+//    }
 }
