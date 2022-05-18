54625a2a0b1dfedc421a9208692a938e23376d30
Zhang Houbin <245605738@qq.com>
2022-05-19 01:41:53 +0800

    他人主页

--- Forum/app/src/main/java/com/example/forum/ui/PersonalPage/OtherHomeActivity.java
+++ Forum/app/src/main/java/com/example/forum/ui/PersonalPage/OtherHomeActivity.java
@@ -2,33 +2,31 @@ package com.example.forum.ui.PersonalPage;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.forum.R;
 import com.example.forum.ui.PersonalPage.PersonHomeAdapter;
 import com.example.forum.ui.moments.SingleMoment;
-
 import android.os.Bundle;
-import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PersonHomeActivity extends AppCompatActivity {
-
+public class OtherHomeActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private PersonHomeAdapter mAdapter;
     List<SingleMoment> momentList=new ArrayList<>();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_person_home);
+        setContentView(R.layout.activity_other_home);
         getSupportActionBar().hide();
+
         recyclerView = findViewById(R.id.person_home_recycler_view);
-        mAdapter = new PersonHomeAdapter(PersonHomeActivity.this,momentList);
+        mAdapter = new PersonHomeAdapter(OtherHomeActivity.this,momentList);
         recyclerView.setAdapter(mAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(PersonHomeActivity.this));
+        recyclerView.setLayoutManager(new LinearLayoutManager(OtherHomeActivity.this));
 
         for (int i = 1; i <= 10; i++) {
             SingleMoment moment = new SingleMoment();
