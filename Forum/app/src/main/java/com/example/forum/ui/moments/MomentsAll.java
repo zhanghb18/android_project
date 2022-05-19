3513d0e0649866ed2c913966c5f9f6dcc8185c06
huangbt19 <hbtvera@163.com>
2022-05-19 15:19:56 +0800

    中期汇报

--- Forum/app/src/main/java/com/example/forum/ui/moments/MomentsAll.java
+++ Forum/app/src/main/java/com/example/forum/ui/moments/MomentsAll.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.forum.bean.GsonFunction;
+import com.example.forum.network.RetrofitUtil;
+import com.example.forum.network.UserAPI;
 import com.example.forum.user.UserApplication;
 
 import androidx.activity.result.ActivityResult;
@@ -18,9 +22,21 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.forum.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MomentsAll extends Fragment {
 
@@ -76,6 +92,50 @@ public class MomentsAll extends Fragment {
                 intentActivityResultLauncher.launch(intent);
             }
         });
+
+        // 获取全部动态列表
+        System.out.println("获取动态");
+        Retrofit retrofit = RetrofitUtil.getRetrofit();
+        UserAPI service = retrofit.create(UserAPI.class);
+        Call<ResponseBody> call = service.GetMoments();
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    JSONObject userInfoRes = GsonFunction.parseToJsonObject(response.body().string());
+                    if (userInfoRes.getBoolean("success")) {
+                        System.out.println("success");
+                        String tmp = userInfoRes.getString("moments");
+                        String[] moments = tmp.split("Moment");
+                        LinkedList<SingleMoment> items = new LinkedList<SingleMoment>();
+                        for(String moment : moments) {
+                            if(moment.endsWith(",")) moment = moment.substring(0, moment.length()-1);
+                            if(!moment.contains("{")) continue;
+                            SingleMoment momentItem = parser(moment);
+                            items.addFirst(momentItem);
+                        }
+                        momentList = items;
+                        System.out.println(momentList);
+                    }
+                    else {
+                        Snackbar.make(view, "Update Failed!", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                        return;
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Snackbar.make(view, "添加失败", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null)
+                        .show();
+            }
+        });
+
 //        for (int i = 1; i <= 10; i++) {
 //            SingleMoment moment = new SingleMoment();
 //            moment.title = "标题" + i;
@@ -84,5 +144,36 @@ public class MomentsAll extends Fragment {
 //        }
         // Inflate the layout for this fragment.
         return view;
+    }
+
+    private SingleMoment parser(String x) {
+        SingleMoment momentItem = null;
+        try {
+            JSONObject jsonObject = GsonFunction.parseToJsonObject(x);
+            boolean flag = false;
+            String email = jsonObject.getString("email");
+            String liked = "";
+            String liked_list = "♥ : ";
+            if(!jsonObject.getString("liked").equals(null)) {
+                liked = jsonObject.getString("liked");
+                liked = liked.substring(1, liked.length() - 1);
+                String[] likeds = liked.split(",");
+                if(liked.length() > 1) {
+                    for(int i = 0; i < likeds.length; i++) {
+                        String tmp = likeds[i].substring(1, likeds[i].length() - 1);
+                        if (tmp.equals(email)){
+                            flag = true;
+                        }
+                        liked_list += tmp + ",";
+                    }
+                }
+            }
+            momentItem = new SingleMoment(email, jsonObject.getString("title"), jsonObject.getString("content"),
+                    jsonObject.getString("post_time"), jsonObject.getString("nickname"), jsonObject.getString("aboutMe"),
+                    liked_list.substring(0, liked_list.length() - 1));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return momentItem;
     }
 }
