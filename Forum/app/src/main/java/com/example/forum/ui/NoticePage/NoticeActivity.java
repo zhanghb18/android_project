847f6b9d9337d018f7ca58dfdf560d0081ab67bb
Zhang Houbin <245605738@qq.com>
2022-06-05 17:41:48 +0800

    一些更改

---
+++ Forum/app/src/main/java/com/example/forum/ui/NoticePage/NoticeActivity.java
@@ -0,0 +1,15 @@
+package com.example.forum.ui.NoticePage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.forum.R;
+
+public class NoticeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice);
+    }
+}
